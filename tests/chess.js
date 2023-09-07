e51ae5b07305a38f0904817ace0fa79451def094
Leonid Gukov <aarbyzevih@egmail.com>
2023-09-08 00:51:37 +0300

    chess was uploded

---
+++ tests/chess.js
@@ -0,0 +1,55 @@
+'use strict';
+
+QUnit.module('Тестируем функцию chess', function () {
+	QUnit.test('Шахматной доски 1 на 1 не бывает', function (assert) {
+		assert.strictEqual(chess(1), null);
+		assert.strictEqual(chess('1'), null);
+	});
+
+	QUnit.test('Шахматная доска 2 на 2', function (assert) {
+		const expected =
+			'* \n' +
+			' *\n';
+		assert.strictEqual(chess(2), expected);
+		assert.strictEqual(chess('2'), expected);
+	});
+
+	QUnit.test('Шахматная доска 3 на 3', function (assert) {
+		const expected =
+			'* *\n' +
+			' * \n' +
+			'* *\n';
+		assert.strictEqual(chess(3), expected);
+		assert.strictEqual(chess('3'), expected);
+	});
+
+	QUnit.test('Шахматная доска 8 на 8', function (assert) {
+		const expected =
+			'* * * * \n' +
+			' * * * *\n' +
+			'* * * * \n' +
+			' * * * *\n' +
+			'* * * * \n' +
+			' * * * *\n' +
+			'* * * * \n' +
+			' * * * *\n';
+		assert.strictEqual(chess(8), expected);
+		assert.strictEqual(chess('8'), expected);
+	});
+
+	QUnit.test('Число с плавающей точкой', function (assert) {
+		const expected = chess(666.666); 
+		assert.strictEqual(expected, null);
+	  });
+
+	  QUnit.test('Отрицательное число', function (assert) {
+		const expected = chess(-89); 
+		assert.strictEqual(expected, null);
+	  });
+
+});
+
+// QUnit.test('Строку нельзя использовать как входные данные', function (assert) {
+// 	const result = chess('ABRAKADABRA'); 
+// 	assert.strictEqual(result, null, 'Строка не пройдёт, только числа');
+//   });
